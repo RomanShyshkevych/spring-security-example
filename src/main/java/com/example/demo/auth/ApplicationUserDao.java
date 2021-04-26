d6da5b192412de2c10e0e1d4b270477862ed7745
Roman <progroman13@gmail.com>
2021-04-26 16:43:08 +0300

    kkk

--- src/main/java/com/example/demo/auth/ApplicationUserDao.java
+++ src/main/java/com/example/demo/auth/ApplicationUserDao.java
@@ -5,4 +5,16 @@ import java.util.Optional;
 public interface ApplicationUserDao {
 
     Optional<ApplicationUser> selectApplicationUserByUserName(String username);
+
+    public static class A {
+
+    }
+
+    private void gen() {
+
+    }
+
+    private static void genr() {
+
+    }
 }
